ad3454f35ebe37fd67fdf9f9bfe8fa2e017819f1
RobertFuel <r.garciax2@gmail.com>
2023-10-01 08:16:11 -0300

    UPDATE: se agrega detail y form

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -8,4 +8,6 @@ app_name = "producto"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="producto/index.html"), name="index"),
     path("productocategoria/list/", views.ProductoCategoriaList.as_view(), name="productocategoria_list"),
+    path("productocategoria/detail/<int:pk>", views.ProductoCategoriaDetail.as_view(), name="productocategoria_detail"),
+    path("productocategoria/create/", views.ProductoCategoriaCreate.as_view(), name="productocategoria_form"),
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
-from . import models
+from . import models, forms
 
 class ProductoCategoriaList(ListView):
     model = models.ProductoCategoria
+    
+class ProductoCategoriaDetail(DetailView):
+    model = models.ProductoCategoria
+    
+class ProductoCategoriaCreate(CreateView):
+    model = models.ProductoCategoria
+    form_class = forms.ProductoCategoriaForm
